e6514344983e4f3bbcfa766fdffa1ee2ed1826da
alessandrovasquez <alessandro.vasquez@outlook.com>
2017-08-07 16:31:55 +0200

    README edited, IoU-score working

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Evaluator On Pixel-Level Semantic Labeling Task
+# Evaluator On Pixel Level Semantic Labeling Task
+
+This script evaluates the predictions of a model that classifies objects in an image through image segmentation. 
+## Input
+The script takes in input two directories' paths, the first containing the predictions and the second the targets. 
+### Assumption
+It is assumed that the prediction and the target are for the same instance are images with the same name and the same dimension in pixel. 
+
+It is not necessary to specify the target classes: the script extrapolate them by creating a dictionary of unique values w.r.t. the colors present in the images.
+
+## Output
+The script outputs a pretty print of two dictionary: the first liks each class (express with the hex value of the associated color) to the IoU score whereas the second one links it to the iIoU-score.
+These scores have been computed by creating two array which contain targets and predictions pixel-by-pixel. 
+
+## Libraries
+Other than numpy and pandas, opencv has been used to load images and sklearn.metrics to compute the confusion matrix 
--- Script.py
+++ Script.py
@@ -1,41 +1,45 @@
 import sys
 import os
-from skimage.io import imread
 import numpy as np
 import pandas as pd
 import time
 from webcolors import rgb_to_hex
 import pprint
+import cv2
 from sklearn import metrics
 
 start_time = time.time()
 # input folders
-#pred_folder_path = sys.argv[1]
-pred_folder_path = '/Users/alessandrovasquez/Desktop/pred/'
-gt_folder_path = '/Users/alessandrovasquez/Desktop/gt/'
-#gt_folder_path = sys.argv[2]
+pred_folder_path = sys.argv[1]
+gt_folder_path = sys.argv[2]
+# pred_folder_path = '/Users/alessandrovasquez/Desktop/pred/'
+# gt_folder_path = '/Users/alessandrovasquez/Desktop/gt/'
 # determine classes
 classes_dict = {}
 y_true = []
 y_pred = []
+instaces_size = []  # array of triple: <class, pred. instance size, target instance size>
 # reading files in predictions' directory
 for file in os.listdir(os.fsencode(gt_folder_path)):
     print(file)
     target = gt_folder_path + os.fsdecode(file)
     if target.endswith('.png'):
-        target_im = imread(target)
+        target_im = cv2.imread(target)
         # assuming that prediction and target files have same names
         prediction = pred_folder_path + os.fsdecode(file)
-        pred_im = imread(prediction)
+        pred_im = cv2.imread(prediction)
         # get classes, encoded predictions and targets
+        # print('starting function')
+        # test = np.apply_along_axis(rgb_to_hex, 2, pred_im)
+        # print("--- %s seconds ---" % (time.time() - start_time))
         for i in range(target_im.shape[0]):
             for j in range(target_im.shape[1]):
                 # getting target pixel
-                target_pixel_value = target_im[i][j][:3]
+                target_pixel_value = target_im[i][j]
                 if not rgb_to_hex(target_pixel_value) in classes_dict:
                     classes_dict[rgb_to_hex(target_pixel_value)] = target_pixel_value
                 y_true.append(rgb_to_hex(target_pixel_value))
-                pred_pixel_value = pred_im[i][j][:3]
+                pred_pixel_value = pred_im[i][j]
                 if not rgb_to_hex(pred_pixel_value) in classes_dict:
                     classes_dict[rgb_to_hex(pred_pixel_value)] = pred_pixel_value
                 y_pred.append(rgb_to_hex(pred_pixel_value))
@@ -50,7 +54,7 @@ df.columns = keys
 df = df.T
 df.columns = keys
 df = df.T
-df.to_csv(path_or_buf='/Users/alessandrovasquez/Desktop/cf.csv')
+df.to_csv(path_or_buf='../cf.csv')
 # true positive
 tp = np.trace(df.as_matrix())
 IoU = {}
@@ -59,11 +63,17 @@ for r in range(df.shape[0]):
     tp = df.ix[r,r]
     fn = df.ix[r,:].sum() - tp
     fp = df.ix[:,r].sum() - tp
-    IoU['IoU of class ' + df.index[r]] = tp / (tp + fn + fp)
-    avg =  (df.ix[r,:].sum()) / (df.ix[:,r].sum())
+    if tp + fn + fp == 0:
+        IoU['IoU of class ' + df.index[r]] = 0
+    else:
+        IoU['IoU of class ' + df.index[r]] = tp / (tp + fn + fp)
+    '''avg = (df.ix[r,:].sum()) / (df.ix[:,r].sum())
     ifn = fn * avg
     ifp = fp * avg
-    iIoU['iIoU of class ' + df.index[r]] = tp / (tp + ifn + ifp)
+    if tp + ifn + ifp == 0:
+        iIoU['iIoU of class ' + df.index[r]] = 0
+    else:
+        iIoU['iIoU of class ' + df.index[r]] = tp / (tp + ifn + ifp)'''
 pprint.pprint(IoU)
 pprint.pprint(iIoU)
 print("--- %s seconds ---" % (time.time() - start_time))
